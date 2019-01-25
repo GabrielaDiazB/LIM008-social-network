d2b361e3079837617efb31f450c60c380dfc8b0f
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-01-25 10:58:08 -0500

    Se añadió la función para poder registrar junto a firebase y guardar la información en la base de datos

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,18 @@
 // aqui exportaras las funciones que necesites
 
-export const myFunction = () => {
-  // aqui tu codigo
+export const signUpFunction = () => {
+
+  const signUp = document.getElementById('sign-up');
+  signUp.addEventListener('click', () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ...
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+  });
 }
