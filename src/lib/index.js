d4d744cb996edc6fb3f08a36b9d14980f758c8c6
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-01-25 19:14:27 -0500

    Se añadió la función para cerrar sesión y se corrigió la función para poder iniciar sesión.

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,31 +2,74 @@
 
 export const signUpFunction = () => {
 
+  // Función para poder Registrarse
+
   const signUp = document.getElementById('sign-up');
-  signUp.addEventListener('click', () => {
+
+  signUp.addEventListener('click', (event) => {
+    event.preventDefault()
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
       // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
-      // ...
+      
       console.log(errorCode);
       console.log(errorMessage);
     });
   });
 
-  const signIn = document.getElementById('sign-in');
-  signIn.addEventListener('click', () =>{
-    const emailSignIn = document.getElementById('email-si').value;
-    const passwordSignIn = document.getElementById('password-si').value;
-    firebase.auth().signInWithEmailAndPassword(emailSignIn, passwordSignIn).catch(function(error) {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ...
-      console.log(errorCode);
-      console.log(errorMessage);
-    });
+  // Función para saber si el usuario está loggeado o no
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // Usuario está loggeado
+      document.getElementById('log-out-box').style.display = 'block';
+      document.getElementById('sign-in-box').style.display = 'none';
+
+      // const displayName = user.displayName;
+      // const email = user.email;
+      // const emailVerified = user.emailVerified;
+      // const photoURL = user.photoURL;
+      // const isAnonymous = user.isAnonymous;
+      // const uid = user.uid;
+      // const providerData = user.providerData;
+
+      const user = firebase.auth().currentUser; 
+      if(user !== null){
+        const email_id = user.email;
+        document.getElementById('user-para').innerHTML = 'Welcome User : ' + email_id
+      } 
+
+    } else {
+      // Usuario ha cerrado sesión
+      document.getElementById('log-out-box').style.display = 'none';
+      document.getElementById('sign-in-box').style.display = 'block';
+    }
   });
+
+  // Función para Iniciar Sesión
+
+  const signIn = document.getElementById('sign-in');
+  signIn.addEventListener('click', (event) => {
+    event.preventDefault()
+    const userEmail = document.getElementById('email-si').value;
+    const userPassword = document.getElementById('password-si').value;
+
+    firebase.auth().signInWithEmailAndPassword(userEmail,userPassword ).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert('Error : ' + errorCode);
+      window.alert('Error : ' + errorMessage);
+    });
+
+  });
+
+  // Función para Cerrar Sesión
+  const logOut = document.getElementById('log-out');
+  logOut.addEventListener('click', () => {
+    firebase.auth().signOut();
+    });
 }
