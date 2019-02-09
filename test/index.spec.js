fcd4bed401ac9e2982ccf829add9ed018ebe4358
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-02-09 12:50:06 -0500

    Se añadio los tests para poder inciciar sesión

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,39 +1,53 @@
-// import MockFirebase from 'mock-cloud-firestore';
-// const fixtureData = {
-//   __collection__: {
-//     posts: {
-//       __doc__: {
-//         abc123: {
-//           content: 'Hola mundo',
-//         },
-//       }
-//     }
-//   }
-// };
+const firebasemock = require('firebase-mock');
+const mockauth = new firebasemock.MockFirebase();
+const mockfirestore = new firebasemock.MockFirestore();
+mockfirestore.autoFlush();
+mockauth.autoFlush();
+global.firebase = firebasemock.MockFirebaseSdk(
+  // use null if your code does not use RTDB
+  path => (path ? mockdatabase.child(path) : null),
+  () => mockauth,
+  () => mockfirestore
+);
+import {
+  checkInFunction,
+  singInFunction,
+  registerFacebookLogIn,
+  registerGoogleLogIn,
+  registerTwitterLogIn  
+} from '../src/controller-function/function-firebase.js';
 
-// global.firebase = new MockFirebase(fixtureData, { isNaiveSnapshotListenerEnabled: true });
+describe('Create an account with email and password', () => {
+  it('debería poder crear un usuario', () => {
+    return checkInFunction('correofake@hotmail.com', '123456')
+      .then((user) => {
+        expect(user.email).toBe('correofake@hotmail.com');
+      });
+  });
+});
+describe('LogIn with email and password', () => {
+  it('debería iniciar sesión', () => {
+    return singInFunction('correofake@hotmail.com', '123456')
+      .then((user) => {
+        expect(user.email).toBe('correofake@hotmail.com');
+      });
+  });
+});
 
-// import { addPostOnSubmit, deletePostOnSubmit } from '../src/view-controller.js';
-// import {getPost} from '../src/controller-function/function-firebase.js';
-// describe('crear post', () => {
-//   it('debería agregar un post', (done) => {
-//     return addPostOnSubmit('Hola mundo')
-//       .then(() => getPost(
-//         (data) => {
-//           const results = data.find((post) => post.content === 'Hola mundo');
-//           expect(results).toBe('Hola mundo');
-//           done();
-//         }
-//       ));
-//   });
-//   it('debería poder eliminar el post indicado', (done) => {
-//     return deletePostOnSubmit('abc123')
-//       .then(() => getPost(
-//         (data) => {
-//           const results = data.find((post) => post.id === 'abc123');
-//           expect(results).toBe(undefined);
-//           done();
-//         }
-//       ));
-//   }); 
-// });
+describe('Facebook authentication', () => {
+  it('debería ser una función', () => {
+    expect(typeof registerFacebookLogIn).toBe('function');
+  });
+});
+
+describe('Google authentication', () => {
+  it('debería ser una función', () => {
+    expect(typeof registerGoogleLogIn).toBe('function');
+  });
+});
+
+describe('Twitter authentication', () => {
+  it('debería ser una función', () => {
+    expect(typeof registerTwitterLogIn).toBe('function');
+  });
+});
